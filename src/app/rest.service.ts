7afeac5be4cf7e6092494cc44db599a00e125aa1
Guillermo <guillermo.moreno@tajamar365.com>
2021-05-02 12:25:32 +0200

    Configure rest service to use prod or test API URL

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,10 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IInstrumento } from 'src/models/instrumento';
 import { IMusico } from 'src/models/musico';
 
-const URL = "https://orquesta-server.azurewebsites.net";
+const URL_PROD = "https://orquesta-server.azurewebsites.net";
+const URL_TEST = "https://localhost:5001"
+const URL = URL_PROD;
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,32 +23,32 @@ export class RestService {
   constructor(private http: HttpClient) { }
 
   getMusicos(): Observable<IMusico[]> {
-    return this.http.get<IMusico[]>(URL+"/api/Musico");
+    return this.http.get<IMusico[]>(URL + "/api/Musico", httpOptions);
   }
 
   getMusico(id: number): Observable<IMusico> {
-    return this.http.get<IMusico>(URL+"/api/Musico/" + id);
+    return this.http.get<IMusico>(URL + "/api/Musico/" + id, httpOptions);
   }
 
   updateMusico(musico: IMusico) {
     console.log("REST Call");
-    this.http.put<IMusico>(URL+"/api/Musico/" + musico.id, musico).subscribe({
+    this.http.put<IMusico>(URL + "/api/Musico/" + musico.id, musico, httpOptions).subscribe({
       next: data => { },
       error: error => { console.error('There was an error!', error); }
     });
   }
 
   getInstrumentos(): Observable<IInstrumento[]> {
-    return this.http.get<IInstrumento[]>(URL+"/api/Instrumento");
+    return this.http.get<IInstrumento[]>(URL + "/api/Instrumento", httpOptions);
   }
 
   getInstrumento(id: number): Observable<IInstrumento> {
-    return this.http.get<IInstrumento>(URL+"/api/Instrumento/" + id);
+    return this.http.get<IInstrumento>(URL + "/api/Instrumento/" + id, httpOptions);
   }
 
   updateInstrumento(instrumento: IInstrumento) {
     console.log("REST Call");
-    this.http.put<IInstrumento>(URL+"/api/Instrumento/" + instrumento.id, instrumento).subscribe({
+    this.http.put<IInstrumento>(URL + "/api/Instrumento/" + instrumento.id, instrumento, httpOptions).subscribe({
       next: data => { },
       error: error => { console.error('There was an error!', error); }
     });
